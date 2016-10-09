a1d73448d06d429078df8b8056c334c2b058178c
Sebastian <darksinkk@gmail.com>
2016-10-09 09:27:13 +0200

    Java Comments

--- ExampleProgram.java
+++ ExampleProgram.java
@@ -1,5 +1,11 @@
 public class ExampleProgram {
   public static void main(String[ ] args) {
     System.out.println("Hello World");
+    
+    // this is a single-line comment
+    /*  This is also a
+    comment spanning
+    multiple lines */
+    /** This is a documentation comment */
   }
 }
