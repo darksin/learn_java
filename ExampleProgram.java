35961155c9f210dc84f303481e53f483c7e11099
Sebastian <darksinkk@gmail.com>
2016-10-09 09:33:30 +0200

    Variables

--- ExampleProgram.java
+++ ExampleProgram.java
@@ -1,11 +1,21 @@
 public class ExampleProgram {
   public static void main(String[ ] args) {
     System.out.println("Hello World");
-    
+
     // this is a single-line comment
     /*  This is also a
     comment spanning
     multiple lines */
     /** This is a documentation comment */
+
+    String name ="David";
+    int age = 42;
+    double score =15.9;
+    char group = 'Z';
+
+    System.out.println("------------");
+    System.out.println("Name: "+name);
+    System.out.println("Age: "+age);
+    System.out.println("Score: "+score+" | Group: "+group);
   }
 }
